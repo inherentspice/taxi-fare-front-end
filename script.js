7a3e9f482be827bed24dc38e72adff183499dea4
Stephen Smith <stephenlehanesmith@gmail.com>
2022-06-29 23:32:28 -0700

    updated url in script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let taxiFareApiUrl = 'http://localhost:8001/predict'; // replace with your API endpoint
+let taxiFareApiUrl = 'https://taxi-image-kr575za6oa-uw.a.run.app/';
 const centralCoordinates = [-74.00597, 40.71427]; // starting position [lng, lat]
 
 if (window.location.href.includes('https://taxifare.lewagon.com')) {
